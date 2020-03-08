e5a73ea72d60cfb0a857d0b0b08f1b5557739b35
rodrigo <rodrigoestef@gmail.com>
2020-03-07 23:24:00 -0300

    rascunho

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const SerialPort = require('serialport')
+
+
+const port = new SerialPort('COM3',{baudRate:3600});
+
+port.on('data', function (data) {
+    console.log('Data:', data.toString('utf8'))
+  })
+
+const api = new express();
+
+const server = require('http').createServer(api)
+
+api.use(express.static(require('path').join(__dirname,'views')))
+api.set('views',require('path').join(__dirname,'views'))
+api.engine('html',require('ejs').renderFile)
+
+
+
+api.get('/api',async (req,res)=>{  
+      
+    port.write('1')
+    res.send('mudou')
+})
+
+
+api.use('/',async (req,res)=>{
+    res.render('index.html')
+})
+
+
+server.listen(3000)
