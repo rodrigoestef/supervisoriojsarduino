7264d4a31f469f15b137db15750b12b9fad8de84
rodrigoestef <rodrigoestef@gmail.com>
2020-03-10 15:23:17 -0300

    criação da view principal

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Switch,Route, Redirect } from 'react-router-dom'
+import Botao from './components/Botao'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default ()=> {
+  document.head.title = 'supervisorio'
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' render={()=><Botao/>}/>
+        <Route path='/' render={()=><Redirect to='/'/>}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ front/src/components/Botao/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './botao.css'
+
+export default () => {
+    return(
+        <div className='container text-center'>
+            <button className='btn btn-danger'>led</button>
+        </div>
+    )
+}
