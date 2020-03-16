3e87cb8e4785a9a4e1f49ec67a79a1f43e10f141
rodrigoestef <rodrigoestef@gmail.com>
2020-03-16 14:14:38 -0300

    socket.io integado à comunicação serial

--- front/src/components/Botao/index.js
+++ front/src/components/Botao/index.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import api from '../../util/api'
+import React, { useState } from 'react'
 import './botao.css'
+import  iconoff from './lightoff.png'
+import  iconon from './light on.png'
+import io from 'socket.io-client'
 
 export default () => {
 
-    const click = async () =>{
-        const {data} = await api.get('/api')
-        // alert(data)
-    }
+    const [light, setLight] = useState(false)
+    const [text, setText] = useState("ligar")
+    
+    const client = io('/')
 
+    client.on('state',data=>{
+      if (data == 1) {
+        setLight(true)
+        setText('desligar')
+      }else{
+        setLight(false)
+        setText('ligar')
+      }
+        
+    })
+
+    const click = async () =>{
+      client.emit('change','change')
+    }
+    const icon = light ? <img src={iconon}/>:<img src={iconoff}/>
     return(
         <div className='container text-center'>
-            <button className='btn btn-danger' onClick={() => click()}>led</button>
+            <div className='teco'>
+                {icon}
+                <br/><br/>
+                <button className='btn btn-danger' onClick={() => click()}>{text}</button>
+            </div>
         </div>
     )
 }
--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 const express = require('express')
 const SerialPort = require('serialport')
 
-
 const port = new SerialPort('COM3',{baudRate:3600});
+
+const api = new express();
+const server = require('http').createServer(api)
+const io = require('socket.io')(server)
 
 port.on('data', function (data) {
     console.log('Data:', data.toString('utf8'))
-  })
-
-const api = new express();
-
-const server = require('http').createServer(api)
-
+})
 api.use(express.static(require('path').join(__dirname,'/front/build')))
 api.set('views',require('path').join(__dirname,'views'))
 api.engine('html',require('ejs').renderFile)
 
+var light = 0
 
-
-api.get('/api',async (req,res)=>{  
-      
-    port.write('1')
-    res.send('mudou')
-    
+io.on('change',socket=>{
+    light = light == 0 ? 1 : 0
 })
+
+io.on('connection',socket =>{
+    socket.emit('state',light)
+    console.log(socket.id)
+
+    socket.on('change',()=>{
+        console.log('1')
+        
+        light = light == 0 ? 1 : 0
+        socket.emit('state',light)
+        port.write('1')
+        socket.broadcast.emit('state',light)
+    })
+
+})
+
+
+
+// api.get('/api',async (req,res)=>{  
+      
+//     port.write('1')
+//     res.send('mudou')
+    
+// })
 
 
 api.use('/',async (req,res)=>{
